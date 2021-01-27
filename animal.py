93276f9f9ba4f4b79fe9b0d987a530d411153bfa
nalidenaidoubao <76151280+nalidenaidoubao@users.noreply.github.com>
2021-01-27 18:35:36 +0800

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,16 @@
+# 通过class关键字，进行定义
+class animal:
+    name = "KOOK"
+    color = "blue"
+    age = 2
+    gender = "female"
+
+
+def shoot(self):
+    print("shooting")
+
+
+def run(self):
+    print("running")
+
+
